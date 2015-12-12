1b8690e0cea99e44ccc10de90a09f1b11335086a
soul <soulduse@naver.com>
2015-12-12 10:52:29 +0900

    장바구니 수량에 따른 가격 총합 동적변동 삭제 기능 추가 .

--- public/js/hello.js
+++ public/js/hello.js
@@ -17,6 +17,30 @@ angular.module('hello', [])
 			$scope.hello.msg = $filter('uppercase')($scope.hello.msg);
 		}
 
+		$scope.remove = function(item){
+			var index = $scope.items.indexOf(item);
+			$scope.items.splice(index, 1);
+		}
+
+		$scope.items = [
+			{
+				title : '볼펜',
+				count : 4,
+				price : 1800
+			},
+			{
+				title : '지우개',
+				count : 1,
+				price : 800
+			},
+			{
+				title : '연필',
+				count : 12,
+				price : 400
+			}
+
+		];
+
 		// $scope.hello.msg = '안녕하세요.' 와 같다.
 
 	});
