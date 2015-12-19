5fd481bb1542acd3de3ba81a632d5238bdb2cd0c
soul <soulduse@naver.com>
2015-12-19 11:44:28 +0900

    페이징 처리함  - 현재 페이징이 제대로 작동되지 않는 현상 (오타 수정하기)

--- public/js/controllers/products.js
+++ public/js/controllers/products.js
@@ -1,10 +1,18 @@
 angular.module('product')
 	.constant('productListActiveClass', 'btn-danger')
-	.controller('productListCtrl', function($scope, $filter, productListActiveClass){
+	.constant('productListPageCount', 3)
+	.controller('productListCtrl', function($scope, $filter, productListActiveClass, productListPageCount){
 	var selectedCategory = null;
+
+	$scope.selectedPage = 1;
+	$scope.pageSize = productListPageCount;
 
 	$scope.selectCategory = function(newCategory){
 		selectedCategory = newCategory;
+	}
+
+	$scope.selectPage = function(newPage){
+		$scope.selectedPage = newPage;
 	}
 
 	$scope.categoryFilterFn = function(product){
@@ -14,4 +22,10 @@ angular.module('product')
 	$scope.getCategoryClass = function(category){
 		return (selectedCategory == category)?productListActiveClass : '';
 	}
+
+	$scope.getPageClass = function(page){
+		return ($scope.selectedPage == page)? productListActiveClass : '';
+	}
+
+	
 });
