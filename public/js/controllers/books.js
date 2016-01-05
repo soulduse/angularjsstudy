a803556ef8c73611bed8de63313f84a58473af1e
soulduse <soulduse@naver.com>
2016-01-06 00:50:17 +0900

    book homework

--- public/js/controllers/books.js
+++ public/js/controllers/books.js
@@ -1,19 +1,25 @@
 angular.module('book')
-.controller('bookListCtrl', function($scope, $filter){
+.constant('bookListActiveClass', 'btn-danger')
+.controller('bookListCtrl', function($scope, $filter, bookListActiveClass){
 	var selectedGrade = null;
 
 	$scope.selectGrade = function(newGrade){
-		console.log('newGrade : '+newGrade );
+		//console.log('newGrade : '+newGrade );
 		selectedGrade = newGrade.length;
 	}
 
 	$scope.selectHome = function(newGrade){
-		console.log('newGrade : '+newGrade );
+		//console.log('newGrade : '+newGrade );
 		selectedGrade = newGrade;
 	}
 
 	$scope.gradeFilterFn = function(book){
-		console.log('book : '+Math.floor(book.grade));
+		//console.log('book : '+Math.floor(book.grade));
 		return selectedGrade == null || Math.floor(book.grade) == selectedGrade;
+	}
+
+	$scope.getGradeClass = function(newGrade){
+		console.log('selectGrade : '+selectedGrade+" / newGrade : "+newGrade);
+		return (selectedGrade == newGrade.length) ? bookListActiveClass : '';
 	}
 });
